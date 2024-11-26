7413db124c9d8dd63adb8cc3f5fda53907d541c1
ToGoRe01 <jsalmanzab@itc.edu.co>
2024-11-26 02:49:15 -0500

    HAHAHAHHAHAHAHHA

--- whatsapp-bot/base-baileys-memory/app.js
+++ whatsapp-bot/base-baileys-memory/app.js
@@ -4,66 +4,152 @@ const QRPortalWeb = require('@bot-whatsapp/portal')
 const BaileysProvider = require('@bot-whatsapp/provider/baileys')
 const MockAdapter = require('@bot-whatsapp/database/mock')
 
-const flowSecundario = addKeyword(['2', 'siguiente']).addAnswer(['📄 Aquí tenemos el flujo secundario'])
+const userResponses = [];  // Array para almacenar las respuestas del usuario
+let salir = true;
 
-const flowDocs = addKeyword(['doc', 'documentacion', 'documentación']).addAnswer(
-    [
-        '📄 Aquí encontras las documentación recuerda que puedes mejorarla',
-        'https://bot-whatsapp.netlify.app/',
-        '\n*2* Para siguiente paso.',
-    ],
-    null,
-    null,
-    [flowSecundario]
-)
-
-const flowTuto = addKeyword(['tutorial', 'tuto']).addAnswer(
-    [
-        '🙌 Aquí encontras un ejemplo rapido',
-        'https://bot-whatsapp.netlify.app/docs/example/',
-        '\n*2* Para siguiente paso.',
-    ],
-    null,
-    (ctx)=>{
-        userResponse = ctx.body; // Guarda la respuesta del usuario.
-        console.log('Respuesta del usuario:', userResponse);
-    },
-    [flowSecundario]
-)
-
-const flowGracias = addKeyword(['gracias', 'grac']).addAnswer(
-    [
-        '🚀 Puedes aportar tu granito de arena a este proyecto',
-        '[*opencollective*] https://opencollective.com/bot-whatsapp',
-        '[*buymeacoffee*] https://www.buymeacoffee.com/leifermendez',
-        '[*patreon*] https://www.patreon.com/leifermendez',
-        '\n*2* Para siguiente paso.',
-    ],
-    null,
-    null,
-    [flowSecundario]
-)
-
-const flowDiscord = addKeyword(['discord']).addAnswer(
-    ['🤪 Únete al discord', 'https://link.codigoencasa.com/DISCORD', '\n*2* Para siguiente paso.'],
-    null,
-    null,
-    [flowSecundario]
-)
-
-const flowPrincipal = addKeyword(['hola', 'ole', 'alo'])
-    .addAnswer('🙌 Hola bienvenido a este *Chatbot*')
-    .addAnswer(
-        [
-            'te comparto los siguientes links de interes sobre el proyecto',
-            '👉 *doc* para ver la documentación',
-            '👉 *gracias*  para ver la lista de videos',
-            '👉 *discord* unirte al discord',
-        ],
-        null,
-        null,
-        [flowDocs, flowGracias, flowTuto, flowDiscord]
+const flowTest = addKeyword(['test', 'cuestionario', 'ansiedad'])
+    .addAction(async (_, { flowDynamic }) => {
+        await flowDynamic('🎉 ¡Hola! Este es un test de ansiedad. Responde con "sí" o "no". ¿Quieres comenzar?');
+    })
+    .addAction(
+        { capture: true }, // Captura la respuesta del usuario
+        async (ctx, { flowDynamic, endFlow }) => {
+            const input = ctx.body?.trim().toLowerCase(); // Normaliza la respuesta del usuario
+            if (['sí', 'si'].includes(input)) {
+                salir = false;// Si responde sí, continúa con la primera pregunta
+                await flowDynamic('📝 Perfecto. Empecemos con la primera pregunta: ¿Has sentido temblor en las piernas?');
+            } else if (['no'].includes(input)) {
+                salir = true;// Si responde no, termina el flujo
+                await flowDynamic('👋 Entendido. Si necesitas ayuda, no dudes en escribirnos.');
+                 // Termina el flujo si la respuesta es "no".
+            } else {
+                // Si la respuesta es algo diferente a sí/no, vuelve a preguntar
+                await flowDynamic('⚠️ Por favor, responde solo con "sí" o "no". ¿Quieres comenzar el test?');
+            }
+        }
     )
+    .addAction(
+        { capture: true }, // Captura la respuesta para la primera pregunta
+        async (ctx, { flowDynamic, endFlow }) => {
+            if(!salir){
+            const input = ctx.body?.trim(); // Captura la respuesta abierta
+            userResponses.push({ pregunta: '¿Has sentido temblor en las piernas?', respuesta: input });
+            await flowDynamic('😌 ¿Te resulta difícil relajarte?');  // Pregunta siguiente
+            }else{
+                return endFlow();
+            }
+        }
+    )
+    .addAction(
+        { capture: true }, // Captura la respuesta para la segunda pregunta
+        async (ctx, { flowDynamic }) => {
+            const input = ctx.body?.trim(); // Captura la respuesta abierta
+            userResponses.push({ pregunta: '¿Te resulta difícil relajarte?', respuesta: input });
+            await flowDynamic('💭 ¿Tienes temor a que ocurra lo peor?');  // Pregunta siguiente
+        }
+    )
+    .addAction(
+        { capture: true }, // Captura la respuesta para la tercera pregunta
+        async (ctx, { flowDynamic }) => {
+            const input = ctx.body?.trim(); // Captura la respuesta abierta
+            userResponses.push({ pregunta: '¿Tienes temor a que ocurra lo peor?', respuesta: input });
+            await flowDynamic('💓 ¿Tienes latidos del corazón fuertes y acelerados?');  // Pregunta siguiente
+        }
+    )
+    .addAction(
+        { capture: true }, // Captura la respuesta para la cuarta pregunta
+        async (ctx, { flowDynamic }) => {
+            const input = ctx.body?.trim(); // Captura la respuesta abierta
+            userResponses.push({ pregunta: '¿Tienes latidos del corazón fuertes y acelerados?', respuesta: input });
+            await flowDynamic('🌀 ¿Te sientes inestable?');  // Pregunta siguiente
+        }
+    )
+    .addAction(
+        { capture: true }, // Captura la respuesta para la quinta pregunta
+        async (ctx, { flowDynamic }) => {
+            const input = ctx.body?.trim(); // Captura la respuesta abierta
+            userResponses.push({ pregunta: '¿Te sientes inestable?', respuesta: input });
+            await flowDynamic('😨 ¿Te sientes atemorizado o asustado?');  // Pregunta siguiente
+        }
+    )
+    .addAction(
+        { capture: true }, // Captura la respuesta para la sexta pregunta
+        async (ctx, { flowDynamic }) => {
+            const input = ctx.body?.trim(); // Captura la respuesta abierta
+            userResponses.push({ pregunta: '¿Te sientes atemorizado o asustado?', respuesta: input });
+            await flowDynamic('😰 ¿Te sientes nervioso?');  // Pregunta siguiente
+        }
+    )
+    .addAction(
+        { capture: true }, // Captura la respuesta para la séptima pregunta
+        async (ctx, { flowDynamic }) => {
+            const input = ctx.body?.trim(); // Captura la respuesta abierta
+            userResponses.push({ pregunta: '¿Te sientes nervioso?', respuesta: input });
+            await flowDynamic('🤲 ¿Has sentido temblores en las manos?');  // Pregunta siguiente
+        }
+    )
+    .addAction(
+        { capture: true }, // Captura la respuesta para la octava pregunta
+        async (ctx, { flowDynamic }) => {
+            const input = ctx.body?.trim(); // Captura la respuesta abierta
+            userResponses.push({ pregunta: '¿Has sentido temblores en las manos?', respuesta: input });
+            await flowDynamic('😟 ¿Te sientes inquieto o inseguro?');  // Pregunta siguiente
+        }
+    )
+    .addAction(
+        { capture: true }, // Captura la respuesta para la novena pregunta
+        async (ctx, { flowDynamic }) => {
+            const input = ctx.body?.trim(); // Captura la respuesta abierta
+            userResponses.push({ pregunta: '¿Te sientes inquieto o inseguro?', respuesta: input });
+            await flowDynamic('😱 ¿Tienes miedo a perder el control?');  // Pregunta siguiente
+        }
+    )
+    .addAction(
+        { capture: true }, // Captura la respuesta para la décima pregunta
+        async (ctx, { flowDynamic }) => {
+            const input = ctx.body?.trim(); // Captura la respuesta abierta
+            userResponses.push({ pregunta: '¿Tienes miedo a perder el control?', respuesta: input });
+            await flowDynamic('😧 ¿Tienes sensación de ahogo?');  // Pregunta siguiente
+        }
+    )
+    .addAction(
+        { capture: true }, // Captura la respuesta para la undécima pregunta
+        async (ctx, { flowDynamic }) => {
+            const input = ctx.body?.trim(); // Captura la respuesta abierta
+            userResponses.push({ pregunta: '¿Tienes sensación de ahogo?', respuesta: input });
+            await flowDynamic('💔 ¿Sientes miedo en general?');  // Pregunta siguiente
+        }
+    )
+    .addAction(
+        { capture: true }, // Captura la respuesta para la duodécima pregunta
+        async (ctx, { flowDynamic }) => {
+            const input = ctx.body?.trim(); // Captura la respuesta abierta
+            userResponses.push({ pregunta: '¿Sientes miedo en general?', respuesta: input });
+            await flowDynamic('💪 ¿Tienes problemas digestivos?');  // Pregunta siguiente
+        }
+    )
+    .addAction(
+        { capture: true }, // Captura la respuesta para la última pregunta
+        async (ctx, { flowDynamic }) => {
+            const input = ctx.body?.trim(); // Captura la respuesta abierta
+            userResponses.push({ pregunta: '¿Tienes problemas digestivos?', respuesta: input });
+            await flowDynamic('✅ Gracias por completar el cuestionario. Si necesitas más ayuda, no dudes en escribirnos.');
+            console.log(userResponses);
+        }
+    );
+
+    
+module.exports = flowTest;
+
+const flowPrincipal = addKeyword('') // El asterisco captura cualquier palabra o mensaje
+    .addAnswer('🙌 Hola bienvenido a este *Chatbot* de autoayuda')
+    .addAnswer('Noto que te quieres suicidar...')
+    .addAnswer('Pues me alegra')
+    .addAnswer('HAHHAHAHAHAHHAAHAHHAHAHAHHAHAHAHAAHHAHAHAHAHHAHAHAHAHHAHAHAAHHAAAHHAHHAHAHAHAHAHHAHAHAHAHAHAHAHAHAHAHHAHAHAAHHAHA',
+        null,
+        null,
+        [flowTest] // Esto sigue activando el siguiente flujo
+    );
 
 const main = async () => {
     const adapterDB = new MockAdapter()
